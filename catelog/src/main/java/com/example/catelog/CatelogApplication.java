ad9846908acbc7f414a050432a358c5f00aa7ab0
rashalismath2 <rashalismath@gmail.com>
2020-02-18 22:26:04 +0530

    eureka added

--- catelog/src/main/java/com/example/catelog/CatelogApplication.java
+++ catelog/src/main/java/com/example/catelog/CatelogApplication.java
@@ -2,10 +2,12 @@ package com.example.catelog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CatelogApplication {
 
 	public static void main(String[] args) {
